ad5dcd553cd15e0a9ba04082029d0eb7e18381e2
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-23 00:44:46 +0530

    Create Quick_Sort.c
    File that included quicksort functions

---
+++ Sorting/Quick_Sort.c
@@ -0,0 +1,38 @@
+
+/*	use while calling in main
+ *	quickSort(ptrArray, 0, size-1);
+ *	size is size of array
+ */
+
+int partition(int arr[], int left, int right)
+{
+	int i, j;
+	int pivot = arr[right];
+	
+	i = left - 1;
+	
+	for(j = left; j <= right-1; j++)
+	{
+		if(arr[j] <= pivot)
+		{
+			i++;
+			if(arr[i] != arr[j])
+				swap(&arr[i], &arr[j]);
+		}
+	}
+	
+	swap(&arr[i+1], &arr[j]);
+	
+	return (i+1);
+}
+
+void quickSort(int arr[], int left, int right)
+{
+	if(left < right)
+	{
+		int pivot = partition(arr, left, right);
+		
+		quickSort(arr, left, pivot - 1);
+		quickSort(arr, pivot + 1, right);
+	}
+}
