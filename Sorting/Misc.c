faaeb7929bdb829cee6b1c29cf72d105129ea7fa
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-31 23:50:23 +0530

    Update Misc.c

--- Sorting/Misc.c
+++ Sorting/Misc.c
@@ -1,25 +1,25 @@
-//function to set array values
 void setArray(int arr[], int size)
 {	
-	int i=0;
-	for(i=0;i<size;i++)
-		scanf("%d",&arr[i]);
+	int i;
+	for(i = 0; i < size; i++)
+		scanf("%d", &arr[i]);
 }
 
-//function to print array
 void getArray(int arr[], int size)
 {
-	int i=0;
+	int i;
+	
 	printf("\n\nArray : ");
-	for(i=0; i<size; i++)
-		printf("%d ",arr[i]);
+	for(i = 0; i < size; i++)
+		printf("%d ", arr[i]);
 }
 
-//function to swap two int
 void swap(int *x, int *y)
 {
-	int temp;
-	temp=*x;
-	*x=*y;
-	*y=temp;
+	if( *x != *y)
+	{
+		int temp = *x;
+		*x = *y;
+		*y = temp;
+	}
 }
