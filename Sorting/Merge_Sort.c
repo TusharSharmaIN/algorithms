925948ec1ac2e93b81af85e94aafe119ff17b56a
Tushar Sharma <imtusharsharma@gmail.com>
2018-10-31 23:48:51 +0530

    Create Merge_Sort.c

---
+++ Sorting/Merge_Sort.c
@@ -0,0 +1,54 @@
+void merge(int arr[], int p, int q, int r)
+{
+	int n1 = q - p + 1;
+	int n2 = r - q;
+
+	int leftArr[n1], rightArr[n2];
+	
+	int i, j, k;
+	
+	for( i = 0; i < n1; i++ )
+		leftArr[i] = arr[p + i];
+	
+	for( j = 0; j < n2; j++ )
+		rightArr[j] = arr[q + j + 1];
+
+	i = j = 0;
+	k = p;
+	
+    while (i < n1 && j < n2) 
+    { 
+        if (leftArr[i] <= rightArr[j]) 
+        { 
+            arr[k] = leftArr[i++];
+        } 
+        else
+        { 
+            arr[k] = rightArr[j++];
+        } 
+        k++; 
+    } 
+
+    while (i < n1) 
+    { 
+        arr[k++] = leftArr[i++];
+    } 
+
+    while (j < n2) 
+    { 
+        arr[k++] = rightArr[j++];
+    }
+}
+
+//  mrgeSort(arr, 0, size);
+
+void mergeSort(int arr[], int p, int r)
+{	
+	if(p < r)
+	{
+		int q = (p + r)/2;
+		mergeSort(arr, p, q);
+		mergeSort(arr, q+1, r);
+		merge(arr, p, q, r);
+	}
+}
