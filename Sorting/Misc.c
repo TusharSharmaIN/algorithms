d3f704d43f0a687dc3748e59212e822538dd9d51
Tushar Sharma <imtusharsharma@gmail.com>
2018-11-13 16:18:20 +0530

    Update Misc.c
    documentation added

--- Sorting/Misc.c
+++ Sorting/Misc.c
@@ -1,3 +1,9 @@
+/*
+ *	function to set values of an array
+ *	@param arr[] array passed
+ *	@param size array size
+ *	@return void
+ */
 void setArray(int arr[], int size)
 {	
 	int i;
@@ -5,6 +11,12 @@ void setArray(int arr[], int size)
 		scanf("%d", &arr[i]);
 }
 
+/*
+ *	function to display values from an array
+ *	@param arr[] array passed
+ *	@param size array size
+ *	@return void
+ */		
 void getArray(int arr[], int size)
 {
 	int i;
@@ -14,6 +26,12 @@ void getArray(int arr[], int size)
 		printf("%d ", arr[i]);
 }
 
+/*
+ *	function to swap two integer
+ *	@param *x address of first number
+ *	@param *y address of second number
+ *	@return void
+ */
 void swap(int *x, int *y)
 {
 	if( *x != *y)
@@ -22,4 +40,20 @@ void swap(int *x, int *y)
 		*x = *y;
 		*y = temp;
 	}
+}
+
+/*
+ *	function to get max element from an array
+ *	@param arr[] integer array passed
+ *	@param size size of array passed
+ *	@return max max elmenet if found
+ */
+int getMax(int arr[], int size)
+{
+    int i, max = arr[0];
+    for(i = 0; i < size; i++)
+        if( max < arr[i] )
+            max = arr[i];
+
+    return max;
 }
