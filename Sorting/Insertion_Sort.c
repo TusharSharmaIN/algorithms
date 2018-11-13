f7d018a370c964f4e7d6d7d1482405844c78c963
Tushar Sharma <imtusharsharma@gmail.com>
2018-11-13 16:40:56 +0530

    Update Insertion_Sort.c
    documentation added

--- Sorting/Insertion_Sort.c
+++ Sorting/Insertion_Sort.c
@@ -1,18 +1,22 @@
-//function to insert sort an array
-//arr[] is the array passed, size is the size of array passed
+/*
+ *	function to sort and array
+ *	@param array to be sorted
+ *	@param size size of an array
+ *	@return void
+ */
 void insertionSort(int arr[], int size)
 {
 	int j;
 	int temp, pos;
-	for(j=1; j<size; j++)
+	for(j = 1; j<size; j++)
 	{
-		temp=arr[j];
-		pos=j-1;
-		while(pos>=0 && arr[pos]>temp)
+		temp = arr[j];
+		pos = j-1;
+		while(pos >= 0 && arr[pos] > temp)
 		{
-			arr[pos+1]=arr[pos];
+			arr[pos+1] = arr[pos];
 			pos--;
 		}
-		arr[pos+1]=temp;
+		arr[pos+1] = temp;
 	}
 }
