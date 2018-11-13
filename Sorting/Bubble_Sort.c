3496a24f75679faf01cda77ad6566b1f596a4168
Tushar Sharma <imtusharsharma@gmail.com>
2018-11-13 16:01:21 +0530

    Update Bubble_Sort.c
    Documentation added

--- Sorting/Bubble_Sort.c
+++ Sorting/Bubble_Sort.c
@@ -1,25 +1,27 @@
-
-//function to bubble sort an array
+/*
+ *	function to sort an array using bubble sort algorithm
+ *	@param arr[] array to be sorted
+ *	@param size size of aray passed
+ *	@return void
+ */
 void bubbleSort(int arr[], int size)
 {
-	int i,j;
+	int i, j;
 	int swp;
 	
-	for(i=0;i<size-1;i++)
+	for(i = 0; i < size-1; i++)
 	{	
 		swp = 0;
 	
-		for(j=0;j<size-i-1;j++)
+		for(j = 0; j < (size-i-1); j++)
 		{	
-			if(arr[j]>arr[j+1])
+			if(arr[j] > arr[j+1])
 			{	
 				swap(&arr[j], &arr[j+1]);
 				swp = 1;
 			}
-		}
-		
+		}	
 		if(!swp)
 			break;
 	}
-	
 }
